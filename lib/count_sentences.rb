aa04a9fba34895d7ef341e879329a80f340fe356
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-16 19:02:33 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
 
   def count_sentences
-    sentences = self.split(/[.?!]/).reject ()
+    sentences = self.split(/[.?!]/).reject {|string| string.empty?
     sentences.count 
   end
 end
